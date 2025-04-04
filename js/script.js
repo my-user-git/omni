d881b0ed7f7874dcd2350e535db435f9e33326ea
geekbraines <morozovol@yandex.ru>
2025-04-04 21:04:13 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,131 @@
+const swiperTop = new Swiper('.section-top__slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  breakpoints: {
+
+    390: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.section-top__next',
+    prevEl: '.section-top__prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const swiperMaybe = new Swiper('.section-maybe__slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  // loop: true,
+
+  breakpoints: {
+
+    390: {
+      slidesPerView: 1
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.section-maybe__next',
+    prevEl: '.section-maybe__prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+document.querySelectorAll('.section-conditions__h2').forEach(button => {
+  button.addEventListener('click', () => {
+    const content = button.nextElementSibling;
+    button.classList.toggle('section-conditions__h2_active');
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
+
+
+document.querySelectorAll('.section-card__tabs-btn').forEach(function (Tabs) {
+  Tabs.addEventListener('click', function (event) {
+    let el = this
+    el.classList.add('section-card__tabs-btn_active')
+    document.querySelectorAll('.section-card__article').forEach(item => {
+      if (item != el) {
+        item.classList.remove('section-card__article_active')
+      }
+    })
+    document.querySelectorAll('.section-card__tabs-btn').forEach(item => {
+      if (item != el) {
+        item.classList.remove('section-card__tabs-btn_active')
+      }
+    })
+    console.log(document.querySelectorAll('.section-card__article'))
+    const path = event.currentTarget.dataset.path
+    document.querySelectorAll('.section-card__article').forEach(function (TabsContent) {
+      TabsContent.classList.remove('section-card__article_active')
+    })
+    document.querySelector(`[data-target="${path}"]`).classList.add('section-card__article_active')
+    console.log(event.currentTarget.dataset.path)
+  })
+})
+
+document.querySelector('.header__burger').addEventListener('click', function () {
+  document.querySelector('.header__menu').classList.add('header__menu-active')
+})
+document.querySelector('.header__burger-x').addEventListener('click', function () {
+  document.querySelector('.header__menu').classList.remove('header__menu-active')
+})
+document.addEventListener('click', e => {
+  const target = e.target
+  if (!target.closest('.header__burger') && !target.closest('.header__burger-x')) {
+    document.querySelector('.header__menu').classList.remove('header__menu-active')
+  }
+})
